6ea9440b52a6a8e04a579384cbaec688d9e31d01
Stefano Frontini <stefanofrontini75@gmail.com>
2024-10-03 11:28:13 +0200

    webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,45 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   transpilePackages: ["@pagopa/mui-italia"],
+  distDir: isProd ? "dist" : undefined,
+  output: "export",
+  assetPrefix: isProd ? "/assets/js/dashboard-new/" : undefined,
+  generateBuildId: () => "fixed-build-id",
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, "canvas", "jQuery"];
+    config.optimization.splitChunks = false;
+    config.plugins.forEach((p) => {
+      // REF: To override plugins config: https://stackoverflow.com/a/59045822
+
+      // Remove style hash
+      if (p.constructor.name === "NextMiniCssExtractPlugin") {
+        // HACK: cannot find the real instance
+        p.options.filename = "static/css/[name].css";
+        p.options.chunkFilename = "static/css/[name].css";
+      }
+
+      // Remove polyfills hash
+      if (
+        p.constructor.name === "CopyFilePlugin" &&
+        p.name === "static/chunks/polyfills-[hash].js"
+      ) {
+        p.name = "static/chunks/polyfills.js";
+      }
+    });
+
+    // Remove javascript chunks hash
+    if (config.output.filename === "static/chunks/[name]-[contenthash].js") {
+      (config.output.filename = "static/chunks/[name].js"),
+        (config.output.chunkFilename = "static/chunks/[name].js");
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
