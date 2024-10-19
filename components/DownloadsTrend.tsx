91b0e9f42d844a1525363a711fa64c9f74b6c801
Stefano Frontini <stefanofrontini75@gmail.com>
2024-10-19 18:14:22 +0200

    disable autofocus

--- components/DownloadsTrend.tsx
+++ components/DownloadsTrend.tsx
@@ -9,7 +9,7 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { TopLevelSpec } from "vega-lite";
 import DownloadsMessagesChart from "./DownloadsMessagesChart";
 import KpiCard from "./KpiCard";
@@ -44,31 +44,31 @@ type Props = {
   title: string;
   spec: TopLevelSpec;
 };
-function usePreventScrollOnFocus() {
-  console.log("usePreventScrollOnFocus");
-  const scrollPos = useRef<[number, number]>([0, 0]);
+// function usePreventScrollOnFocus() {
+//   console.log("usePreventScrollOnFocus");
+//   const scrollPos = useRef<[number, number]>([0, 0]);
 
-  useEffect(() => {
-    const scrollHandler = () => {
-      scrollPos.current[0] = window.scrollX;
-      scrollPos.current[1] = window.scrollY;
-      console.log("scrollHandler", ...scrollPos.current);
-    };
-    window.addEventListener("scroll", scrollHandler);
-    return () => window.removeEventListener("scroll", scrollHandler);
-  }, []);
+//   useEffect(() => {
+//     const scrollHandler = () => {
+//       scrollPos.current[0] = window.scrollX;
+//       scrollPos.current[1] = window.scrollY;
+//       console.log("scrollHandler", ...scrollPos.current);
+//     };
+//     window.addEventListener("scroll", scrollHandler);
+//     return () => window.removeEventListener("scroll", scrollHandler);
+//   }, []);
 
-  const handleFocus = useCallback(() => {
-    window.scrollTo(...scrollPos.current);
-    console.log("scrollTo", ...scrollPos.current);
-  }, []);
+//   const handleFocus = useCallback(() => {
+//     window.scrollTo(...scrollPos.current);
+//     console.log("scrollTo", ...scrollPos.current);
+//   }, []);
 
-  return handleFocus;
-}
+//   return handleFocus;
+// }
 
 const DownloadsMessagesTrend = ({ idChart, title, spec }: Props) => {
   const [curOptionYear, setCurOptionYear] = useState(optionsYear[0].id);
-  const preventScrollOnFocus = usePreventScrollOnFocus();
+  // const preventScrollOnFocus = usePreventScrollOnFocus();
 
   const handleOptionsYear = (id: number) => {
     setCurOptionYear(id);
@@ -102,15 +102,16 @@ const DownloadsMessagesTrend = ({ idChart, title, spec }: Props) => {
         </Typography>
         <FormControl>
           <Select
-            onClick={preventScrollOnFocus}
-            onFocus={preventScrollOnFocus}
+            // onFocus={preventScrollOnFocus}
+
             IconComponent={ExpandMoreOutlinedIcon}
             id={idChart}
             inputProps={{ id: idChart + "-input" }}
             value={curOptionYear}
             size="small"
             MenuProps={{
-              onFocus: preventScrollOnFocus,
+              disableAutoFocus: true,
+              disableEnforceFocus: true,
             }}
             onChange={(e: SelectChangeEvent<number>) =>
               handleOptionsYear(+e.target.value)
